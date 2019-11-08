fd649e691b34b82102240b4f2e6c6d9b399fdf91
FakeJoker <olivertweesty32@gmail.com>
2019-11-08 19:52:46 +0300

    Update Database.py

--- utils/Database.py
+++ utils/Database.py
@@ -5,7 +5,7 @@ import utils.tables as tb
 class Database:
 	def __init__(self, databasename,_password):
 		self.databasename = databasename
-		self.password = "9993revilo"
+		self.password = "9993Rev!lo"
 		#Creating the database if it does not exist
 		connection = pymysql.connect(host='localhost',
                              user='root',
